c8bd9ff698c3f301fd8c03589f031d5c97fb8e1f
Shane Lillie <ignignokterr@gmail.com>
2026-02-04 18:38:09 -0800

    Ok, this works for now

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Bevy Mixamo example
+
+* https://github.com/facebookincubator/FBX2glTF v0.9.7
+  * ./FBX2glTF-linux-x64 -b assets/models/Mutant.fbx --output assets/models/Mutant.glb
+  * I don't know if the .fbm directory with the textures in it is necessary to keep
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,19 @@
-use bevy::{color::palettes::css::*, prelude::*};
-use bevy_ufbx::FbxPlugin;
+use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(AssetPlugin {
-            // TODO: this isn't great but fbx's won't load without it
-            unapproved_path_mode: bevy::asset::UnapprovedPathMode::Allow,
-            ..default()
-        }))
+        .add_plugins(DefaultPlugins)
         .add_plugins(bevy::remote::RemotePlugin::default())
         .add_plugins(bevy::remote::http::RemoteHttpPlugin::default())
-        .add_plugins(FbxPlugin)
         .add_systems(Startup, setup)
-        .add_systems(Update, apply_fallback_material)
         .run();
 }
 
-#[derive(Component)]
-struct FallbackMaterialApplied;
-
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: Res<AssetServer>,
-) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Camera
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_xyz(0.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         AmbientLight {
             color: Color::WHITE,
             brightness: 200.0,
@@ -53,39 +38,13 @@ fn setup(
         Name::new("Directional Light"),
     ));
 
-    // Test cube
-    /*commands.spawn((
-        Mesh3d(meshes.add(Cuboid::default())),
-        MeshMaterial3d(materials.add(Color::from(ORANGE))),
-        Transform::from_xyz(0.0, 0.5, 0.0),
-    ));*/
-
-    // Load FBX scene
-    let scene = asset_server.load::<Scene>("models/Mutant.fbx#Scene0");
+    // Load GLB scene
+    let scene = asset_server.load::<Scene>("models/Mutant.glb#Scene0");
 
     // Spawn the scene
     commands.spawn((
         SceneRoot(scene),
-        Transform::from_xyz(0.0, 0.5, 0.0).with_scale(Vec3::splat(100.0)),
+        Transform::from_xyz(0.0, 0.0, 0.0),
         Name::new("Mutant"),
     ));
-}
-
-fn apply_fallback_material(
-    mut commands: Commands,
-    query: Query<(Entity, &MeshMaterial3d<StandardMaterial>), (With<Mesh3d>, Without<FallbackMaterialApplied>)>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    for (entity, material_handle) in &query {
-        if let Some(material) = materials.get_mut(&material_handle.0) {
-            material.base_color = Color::WHITE;
-            material.base_color_texture = None;
-            material.emissive = LinearRgba::BLACK;
-            material.alpha_mode = AlphaMode::Opaque;
-            material.double_sided = true;
-            material.cull_mode = None;
-            material.unlit = true;
-        }
-        commands.entity(entity).insert(FallbackMaterialApplied);
-    }
 }
